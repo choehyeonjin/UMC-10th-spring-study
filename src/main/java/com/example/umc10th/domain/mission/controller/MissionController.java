a9c3e9d0a0945289d95e9c137b2536912163ffce
choehyeonjin <guswls2616@naver.com>
2026-04-26 23:07:53 +0900

    feat: 미션 API controller, dto 추가

--- src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
@@ -1,4 +1,55 @@
 package com.example.umc10th.domain.mission.controller;
 
+import com.example.umc10th.domain.home.exception.code.HomeSuccessCode;
+import com.example.umc10th.domain.mission.dto.MissionReqDTO;
+import com.example.umc10th.domain.mission.dto.MissionResDTO;
+import com.example.umc10th.domain.mission.exception.code.MissionSuccessCode;
+import com.example.umc10th.domain.mission.service.MissionService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class MissionController {
+
+    // private final MissionService missionService;
+
+    // 미션 목록 조회
+    @GetMapping("/v1/missions")
+    public ApiResponse<MissionResDTO.MissionListDTO> getMissions(
+            @RequestParam String status,
+            @RequestParam(defaultValue = "0") Integer page
+    ) {
+        BaseSuccessCode code = MissionSuccessCode.OK;
+        return ApiResponse.onSuccess(
+                code,
+                null // missionService
+        );
+    }
+
+    // 미션 도전
+    @PostMapping("/v1/missions/{missionId}/challenge")
+    public ApiResponse<MissionResDTO.MissionChallengeDTO> challengeMission(
+            @PathVariable Long missionId
+    ) {
+        BaseSuccessCode code = MissionSuccessCode.CHALLENGE_OK;
+        return ApiResponse.onSuccess(
+                code,
+                null // missionService
+        );
+    }
+
+    // 미션 성공
+    @PatchMapping("/v1/member-missions/{memberMissionId}/success")
+    public ApiResponse<MissionResDTO.MissionSuccessDTO> completeMission(
+            @PathVariable Long memberMissionId
+    ) {
+        BaseSuccessCode code = MissionSuccessCode.SUCCESS_OK;
+        return ApiResponse.onSuccess(
+                code,
+                null); // missionService
+    }
 }
--- src/main/java/com/example/umc10th/domain/mission/dto/MissionResDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionResDTO.java
@@ -1,4 +1,46 @@
 package com.example.umc10th.domain.mission.dto;
 
+import com.example.umc10th.global.dto.PageResDTO.PageInfoDTO;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MissionResDTO {
+
+    // 미션 목록 조회
+    @Builder
+    public record MissionListDTO(
+            PageInfoDTO pageInfo,
+            List<MissionDetailDTO> missions
+    ) {}
+
+    @Builder
+    public record MissionDetailDTO(
+            Long memberMissionId,
+            Long missionId,
+            Long storeId,
+            String status,
+            String storeName,
+            Integer missionPoint,
+            String missionCondition,
+            Boolean reviewWritten)
+    {}
+
+    // 미션 도전
+    @Builder
+    public record MissionChallengeDTO(
+            Long memberMissionId,
+            Long missionId,
+            String status,
+            LocalDateTime createdAt)
+    {}
+
+    // 미션 성공
+    @Builder
+    public record MissionSuccessDTO(
+            Long memberMissionId,
+            String status,
+            LocalDateTime updatedAt
+    ) {}
 }
