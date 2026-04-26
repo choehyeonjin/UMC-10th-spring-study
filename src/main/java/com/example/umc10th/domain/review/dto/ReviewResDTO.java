78b56199b8d3677caa08e863ce111348f223e8f4
choehyeonjin <guswls2616@naver.com>
2026-04-26 23:07:01 +0900

    feat: 리뷰 작성 API controller, dto 추가

--- src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
+++ src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
@@ -1,4 +1,15 @@
 package com.example.umc10th.domain.review.dto;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
 public class ReviewResDTO {
+
+    // 리뷰 작성
+    @Builder
+    public record CreateReviewResultDTO(
+            Long reviewId,
+            LocalDateTime createdAt
+    ) {}
 }
--- src/main/java/com/example/umc10th/domain/review/exception/code/ReviewSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/review/exception/code/ReviewSuccessCode.java
@@ -1,4 +1,20 @@
 package com.example.umc10th.domain.review.exception.code;
 
-public enum ReviewSuccessCode {
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ReviewSuccessCode implements BaseSuccessCode {
+
+    CREATED(HttpStatus.CREATED,
+            "REVIEW201_1",
+            "리뷰 작성에 성공했습니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
